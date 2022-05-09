dd553619e2d70950686c68587a87566ce2f33af4
Rupert Adlmaier <rupert@any.studio>
2022-05-09 17:40:33 +0200

    use elastic

--- components/Header/HeaderContainer.js
+++ components/Header/HeaderContainer.js
@@ -8,15 +8,15 @@ const HeaderContainer = () => {
 
   const animRefs = useRef({})
   const backgroundRef = useRef(null)
-  const indexRefs = useRef({})
-  const titleRefs = useRef({})
+  const indexRefs = useRef([])
+  const titleRefs = useRef([])
 
   useEffect(() => {
     animRefs.current.timeline = gsap
-      .timeline({ paused: true, defaults: { duration: 1, ease: 'none' } })
-      .fromTo(Object.values(titleRefs.current), { opacity: 1 }, { opacity: 0, duration: 0.15, stagger: 0.05 })
+      .timeline({ paused: true, defaults: { duration: 1, ease: 'strong.inOut' } })
+      .fromTo(titleRefs.current, { opacity: 1 }, { opacity: 0, duration: 0.15, stagger: 0 })
       .fromTo(backgroundRef.current, { y: 0 }, { y: -100, duration: 0.15 })
-      .fromTo(Object.values(indexRefs.current), { opacity: 1 }, { opacity: 0, duration: 0.2, stagger: 0.1 })
+      .fromTo(indexRefs.current, { x: 0 }, { x: (index) => index * -110, duration: 0.5, ease: 'elastic.inOut(1, 0.75)' })
 
     return () => {
       animRefs.current.timeline.kill()
