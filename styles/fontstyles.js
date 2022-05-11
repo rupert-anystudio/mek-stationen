f974e34f020057dd50296c9f98eb638a9913ff1e
Rupert Adlmaier <rupert@any.studio>
2022-05-11 09:23:45 +0200

    add fonts

--- styles/fontstyles.js
+++ styles/fontstyles.js
@@ -1,9 +1,14 @@
 const fontstyles = {
   root: `
-    font-family: Helvetica, Arial, sans-serif;
+    font-family: acuminpro, Helvetica, Arial, sans-serif;
     font-weight: normal;
     font-style: normal;
-  `
+  `,
+  title: `
+    font-family: balboabold, Helvetica, Arial, sans-serif;
+    font-weight: normal;
+    font-style: normal;
+  `,
 }
 
 export default fontstyles
