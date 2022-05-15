9e8bf99f859dc3c8590575cc752e5eb096806e30
Rupert Adlmaier <rupert@any.studio>
2022-05-15 21:46:10 +0200

    useWindowScrollDirection hook

---
+++ components/useWindowScrollDirection.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+
+const useWindowScrollDirection = () => {
+  const [scrollDir, setScrollDir] = useState('UP')
+
+  useEffect(() => {
+    const threshold = 50
+    let lastScrollY = window.pageYOffset
+    let ticking = false
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false
+        return
+      }
+      setScrollDir(scrollY > lastScrollY ? 'DOWN' : 'UP')
+      lastScrollY = scrollY > 0 ? scrollY : 0
+      ticking = false
+    }
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir)
+        ticking = true
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+    console.log(scrollDir)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [scrollDir])
+
+  return scrollDir
+}
+
+export default useWindowScrollDirection
