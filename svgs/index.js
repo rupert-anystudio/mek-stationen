6af79ced9e135cc2677c27dbfb97b06ea324cb8c
Rupert Adlmaier <rupert@any.studio>
2022-05-18 17:59:09 +0200

    added scrollpfeil svg

--- svgs/index.js
+++ svgs/index.js
@@ -1,5 +1,6 @@
 export { default as Decrease } from "./Decrease";
 export { default as Increase } from "./Increase";
 export { default as Play } from "./Play";
+export { default as Scrollpfeil } from "./Scrollpfeil";
 export { default as Stop } from "./Stop";
 export { default as Volume } from "./Volume";
